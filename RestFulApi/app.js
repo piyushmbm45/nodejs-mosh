e983388c4991e0f0bcf3325bd8c00c2a54bc7882
piyushmbm45 <piyushmbm45@gmail.com>
2021-11-28 16:32:08 +0530

    first server

---
+++ RestFulApi/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(3000, () => console.log(`listening on Port 3000`));
