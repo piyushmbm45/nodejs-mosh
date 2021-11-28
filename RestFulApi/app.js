f98f9c28507a04bc7ea5d2e4dad1a5f5b1caba71
piyushmbm45 <piyushmbm45@gmail.com>
2021-11-28 18:17:21 +0530

    finding course using route params

--- RestFulApi/app.js
+++ RestFulApi/app.js
@@ -4,18 +4,26 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 3" },
+];
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3,4])
-})
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get('/api/courses/:id', (req,res)=>{
-    const queryParams = req.query;
-    const courseId = req.params;
-    res.send({courseId, queryParams});
-})
+app.get("/api/courses/:id", (req, res) => {
+  const courseId = req.params.id;
+  const foundCourse = courses.find((ele) => ele.id === parseInt(courseId));
+  if(!foundCourse){
+      res.status(404).send(`Course not found`)
+  }
+  res.send(foundCourse);
+});
 
 app.listen(port, () => console.log(`listening on Port ${port}`));
