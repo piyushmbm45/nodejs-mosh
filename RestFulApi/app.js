705850636295dcadfaa6370042336ec55f0c765d
piyushmbm45 <piyushmbm45@gmail.com>
2021-11-28 19:15:45 +0530

    put request

--- RestFulApi/app.js
+++ RestFulApi/app.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+const Joi = require("joi");
 const express = require("express");
 const port = process.env.PORT || 3000;
 
@@ -20,30 +20,49 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const courseId = req.params.id;
   const foundCourse = courses.find((ele) => ele.id === parseInt(courseId));
-  if(!foundCourse){
-      res.status(404).send(`Course not found`)
+  if (!foundCourse) {
+    res.status(404).send(`Course not found`);
   }
   res.send(foundCourse);
 });
 
-app.post('/api/courses',(req,res)=>{
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const result = schema.validate(req.body);
+  console.log(result);
+  if (result.error) {
+    // 400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const newCourse = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(newCourse);
+  res.send(newCourse);
+});
 
-    const schema = Joi.object({
-        name : Joi.string().min(3).required()
-    });
-    const result = schema.validate(req.body)
-    console.log(result);
-    if(result.error){
-        // 400 bad request
-        res.status(400).send(result.error.details[0].message)
-        return;
-    }
-    const newCourse = {
-        id : courses.length + 1,
-        name : req.body.name
-    }
-    courses.push(newCourse)
-    res.send(newCourse);
-})
+app.put("/api/courses/:id", (req, res) => {
+  const foundCourse = courses.find((ele) => ele.id === parseInt(req.params.id));
+  if (!foundCourse) {
+    res.status(400).send("no course available");
+    return;
+  }
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const result = schema.validate(req.body);
+//   console.log(result);
+  if (result.error) {
+    // 400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  foundCourse.name = req.body.name;
+  res.send(foundCourse);
+});
 
 app.listen(port, () => console.log(`listening on Port ${port}`));
